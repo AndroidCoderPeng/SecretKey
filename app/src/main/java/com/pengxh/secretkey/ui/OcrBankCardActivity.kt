7c17f49f0264ead6eb768014affa45375b4f1b4a
pengxianhong <290677893@qq.com>
2020-11-18 23:04:49 +0800

    新增银行卡识别模块功能，细节待后续完善

--- app/src/main/java/com/pengxh/secretkey/ui/OcrBankCardActivity.kt
+++ app/src/main/java/com/pengxh/secretkey/ui/OcrBankCardActivity.kt
@@ -1,7 +1,19 @@
 package com.pengxh.secretkey.ui
 
+import android.graphics.Bitmap
+import android.util.Log
+import com.baidu.ocr.sdk.OCR
+import com.baidu.ocr.sdk.OnResultListener
+import com.baidu.ocr.sdk.exception.OCRError
+import com.baidu.ocr.sdk.model.BankCardParams
+import com.baidu.ocr.sdk.model.BankCardResult
+import com.google.gson.Gson
 import com.pengxh.app.multilib.base.BaseNormalActivity
+import com.pengxh.app.multilib.widget.EasyToast
 import com.pengxh.secretkey.R
+import com.pengxh.secretkey.utils.CameraPreviewHelper
+import kotlinx.android.synthetic.main.activity_ocr.*
+import java.io.File
 
 /**
  * @description: TODO
@@ -9,12 +21,14 @@ import com.pengxh.secretkey.R
  * @email: 290677893@qq.com
  * @date: 2020/11/17 23:13
  */
-class OcrBankCardActivity : BaseNormalActivity() {
+class OcrBankCardActivity : BaseNormalActivity(), CameraPreviewHelper.OnCaptureImageCallback {
 
     companion object {
         private const val TAG = "OcrBankCardActivity"
         const val resultCode = 888
     }
+
+    private lateinit var cameraPreviewHelper: CameraPreviewHelper
 
     override fun initLayoutView(): Int = R.layout.activity_ocr
 
@@ -24,5 +38,31 @@ class OcrBankCardActivity : BaseNormalActivity() {
 
     override fun initEvent() {
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        cameraPreviewHelper = CameraPreviewHelper(this, targetPreView)
+        cameraPreviewHelper.setImageCallback(this)
+        recognizeButton.setOnClickListener {
+            Log.d(TAG, "initEvent: ")
+            cameraPreviewHelper.takePicture()
+        }
+    }
+
+    override fun captureImage(localPath: String?, bitmap: Bitmap?) {
+        val param = BankCardParams()
+        param.imageFile = File(localPath!!)
+        OCR.getInstance(this).recognizeBankCard(param, object : OnResultListener<BankCardResult> {
+            override fun onResult(bankCardResult: BankCardResult?) {
+                Log.d(TAG, "onResult: " + Gson().toJson(bankCardResult))
+            }
+
+            override fun onError(ocrError: OCRError?) {
+                if (ocrError != null) {
+                    EasyToast.showToast("识别失败，请重试", EasyToast.WARING)
+                }
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/pengxh/secretkey/utils/CameraPreviewHelper.kt
@@ -0,0 +1,306 @@
+package com.pengxh.secretkey.utils
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
+import android.hardware.camera2.CameraCaptureSession.CaptureCallback
+import android.media.ImageReader
+import android.media.ImageReader.OnImageAvailableListener
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import android.view.TextureView.SurfaceTextureListener
+import androidx.core.content.ContextCompat
+import com.pengxh.app.multilib.utils.DensityUtil
+import com.pengxh.app.multilib.widget.EasyToast
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+/**
+ * @author: Pengxh
+ * @email: 290677893@qq.com
+ * @description: TODO 相机部分功能封装
+ * @date: 2020年11月16日19:26:32
+ */
+class CameraPreviewHelper(private val mContext: Context, textureView: TextureView) {
+    private val mTextureView: TextureView = textureView
+    private val mPreviewHandler: Handler
+    private var mCameraId = "0"
+    private var mCharacteristics: CameraCharacteristics? = null
+    private var mSensorOrientation: Int? = null
+    private var mImageReader: ImageReader? = null
+    private var mCameraDevice: CameraDevice? = null
+    private var mCaptureSession: CameraCaptureSession? = null
+    private var canTakePic = false
+    private var imageCallback: OnCaptureImageCallback? = null
+    private var mPreviewSize = Size(
+        DensityUtil.getDisplaySize(mContext)["HorizontalPixels"]!!,
+        DensityUtil.getDisplaySize(mContext)["VerticalPixels"]!!
+    )
+
+    fun setImageCallback(callback: OnCaptureImageCallback?) {
+        imageCallback = callback
+    }
+
+    companion object {
+        private const val TAG = "CameraPreviewHelper"
+        private const val mCameraFacing = CameraCharacteristics.LENS_FACING_BACK //默认使用后置摄像头
+    }
+
+    init {
+        //打开相机和创建会话等都是耗时操作，所以我们启动一个HandlerThread在子线程中来处理
+        val mPreviewThread = HandlerThread("CameraPreviewThread")
+        mPreviewThread.start()
+        mPreviewHandler = Handler(mPreviewThread.looper)
+        mTextureView.surfaceTextureListener = object : SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                try {
+                    initCamera()
+                } catch (e: CameraAccessException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                stopPreview()
+                return true
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
+        }
+    }
+
+    /**
+     * 初始化相机
+     */
+    @Throws(CameraAccessException::class)
+    private fun initCamera() {
+        val mCameraManager = (mContext.getSystemService(Context.CAMERA_SERVICE) as CameraManager)
+        val cameraIdList = mCameraManager.cameraIdList
+        if (cameraIdList.isEmpty()) {
+            EasyToast.showToast("没有可用相机", EasyToast.WARING)
+            return
+        }
+        for (id in cameraIdList) {
+            val characteristics = mCameraManager.getCameraCharacteristics(id)
+            val facing = characteristics.get(CameraCharacteristics.LENS_FACING)!!
+            if (mCameraFacing == facing) {
+                mCameraId = id
+                mCharacteristics = characteristics
+            }
+            Log.d(TAG, "设备中的摄像头: $id")
+        }
+        //获取摄像头方向
+        mSensorOrientation = mCharacteristics!!.get(CameraCharacteristics.SENSOR_ORIENTATION)
+        mTextureView.surfaceTexture.setDefaultBufferSize(mPreviewSize.width, mPreviewSize.height)
+        mImageReader =
+            ImageReader.newInstance(mPreviewSize.width, mPreviewSize.height, ImageFormat.JPEG, 1)
+        mImageReader?.setOnImageAvailableListener(mImageAvailableListener, mPreviewHandler)
+        //打开箱机
+        if (ContextCompat.checkSelfPermission(
+                mContext,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            EasyToast.showToast("没有相机权限", EasyToast.WARING)
+            return
+        }
+        mCameraManager.openCamera(mCameraId, object : CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+                Log.d(TAG, "onOpened: " + camera.id)
+                mCameraDevice = camera
+                try {
+                    createCaptureSession(camera)
+                } catch (e: CameraAccessException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                Log.d(TAG, "onDisconnected: " + camera.id)
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+                Log.d(TAG, "onError: " + camera.id)
+                EasyToast.showToast("打开相机失败，错误码：$error", EasyToast.ERROR)
+            }
+        }, mPreviewHandler)
+    }
+
+    /**
+     * 创建预览会话
+     */
+    @Throws(CameraAccessException::class)
+    private fun createCaptureSession(camera: CameraDevice) {
+        val captureRequestBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW) //预览
+        val surface = Surface(mTextureView.surfaceTexture)
+        captureRequestBuilder.addTarget(surface) //将CaptureRequest的构建器与Surface对象绑定在一起
+        captureRequestBuilder.set(
+            CaptureRequest.CONTROL_AE_MODE,
+            CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH
+        ) // 闪光灯
+        captureRequestBuilder.set(
+            CaptureRequest.CONTROL_AF_MODE,
+            CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE
+        ) // 自动对焦
+
+        //为相机预览，创建一个CameraCaptureSession对象
+        val surfaceList = listOf(surface, mImageReader!!.surface) //画面帧集合
+        camera.createCaptureSession(surfaceList, object : CameraCaptureSession.StateCallback() {
+            override fun onConfigured(session: CameraCaptureSession) {
+                mCaptureSession = session
+                try {
+                    session.setRepeatingRequest(
+                        captureRequestBuilder.build(),
+                        mCaptureCallBack,
+                        mPreviewHandler
+                    )
+                } catch (e: CameraAccessException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onConfigureFailed(session: CameraCaptureSession) {
+                EasyToast.showToast("开启预览会话失败！", EasyToast.ERROR)
+            }
+        }, mPreviewHandler)
+    }
+
+    /**
+     * 预览回调
+     */
+    private val mCaptureCallBack: CaptureCallback = object : CaptureCallback() {
+        override fun onCaptureCompleted(
+            session: CameraCaptureSession,
+            request: CaptureRequest,
+            result: TotalCaptureResult
+        ) {
+            super.onCaptureCompleted(session, request, result)
+            canTakePic = true
+        }
+
+        override fun onCaptureFailed(
+            session: CameraCaptureSession,
+            request: CaptureRequest,
+            failure: CaptureFailure
+        ) {
+            super.onCaptureFailed(session, request, failure)
+            Log.d(TAG, "onCaptureFailed: $failure")
+            EasyToast.showToast("开启预览失败！", EasyToast.ERROR)
+            canTakePic = false
+        }
+    }
+
+    /**
+     * 拍照
+     */
+    fun takePicture() {
+        Log.d(TAG, "takePicture: 拍照")
+        if (mCameraDevice == null || !mTextureView.isAvailable) return
+        if (canTakePic) {
+            try {
+                val captureRequestBuilder =
+                    mCameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE) //拍照
+                captureRequestBuilder.addTarget(mImageReader!!.surface)
+                captureRequestBuilder.set(
+                    CaptureRequest.CONTROL_AF_MODE,
+                    CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE
+                ) // 自动对焦
+                captureRequestBuilder.set(
+                    CaptureRequest.CONTROL_AE_MODE,
+                    CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH
+                ) // 闪光灯
+                captureRequestBuilder.set(
+                    CaptureRequest.JPEG_ORIENTATION,
+                    mSensorOrientation
+                ) ////根据摄像头方向对保存的照片进行旋转，使其为"自然方向"
+                mCaptureSession!!.capture(captureRequestBuilder.build(), null, mPreviewHandler)
+            } catch (e: CameraAccessException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private val mImageAvailableListener = OnImageAvailableListener { reader ->
+        val image = reader.acquireNextImage()
+        val byteBuffer = image.planes[0].buffer
+        val bytes = ByteArray(byteBuffer.remaining())
+        byteBuffer[bytes]
+        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+
+        /**
+         * 图片处理
+         */
+        val parentPath = mContext.filesDir.toString() + File.separator
+        val file = File(parentPath)
+        if (!file.exists()) {
+            file.mkdir()
+        }
+        val mPictureFile = File(parentPath, System.currentTimeMillis().toString() + ".jpg")
+        var outputStream: FileOutputStream? = null
+        try {
+            outputStream = FileOutputStream(mPictureFile)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 80, outputStream) //压缩
+            outputStream.flush()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        //直接将bitmap回调到需要的地方
+        imageCallback!!.captureImage(mPictureFile.absolutePath, bitmap)
+        image.close()
+    }
+
+    /**
+     * 回调拍照本地路径和Bitmap
+     */
+    interface OnCaptureImageCallback {
+        fun captureImage(localPath: String?, bitmap: Bitmap?)
+    }
+
+    /**
+     * 停止预览
+     */
+    fun stopPreview() {
+        Log.d(TAG, "stopPreview: 停止预览")
+        if (mCaptureSession != null) {
+            mCaptureSession!!.close()
+            mCaptureSession = null
+        }
+        if (mCameraDevice != null) {
+            mCameraDevice!!.close()
+            mCameraDevice = null
+        }
+        if (mImageReader != null) {
+            mImageReader!!.close()
+            mImageReader = null
+        }
+    }
+}
